d06da4433914be54f63cc13da03cdda4ef2bfc40
石玉龙 <iosshiyulong@yeah.net>
2021-01-05 17:11:53 +0800

    Modify and Style Views

--- SwiftUIPro/SwiftUIPro/MeetingView.swift
+++ SwiftUIPro/SwiftUIPro/MeetingView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value: 5, total: 15)
-        HStack{
-            Text("Seconds Elapsed")
+        VStack{
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage:"hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing){
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage:"hourglass.tophalf.fill")
+                }
+            }
         }
     }
 }
