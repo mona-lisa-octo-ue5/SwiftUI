0d873c589082aad40b6d85b6e0fa318e5d64e05a
石玉龙 <iosshiyulong@yeah.net>
2021-01-05 15:05:46 +0800

    SwiftUIPro

---
+++ SwiftUIPro/SwiftUIPro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPro
+//
+//  Created by 石玉龙 on 2021/1/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPro/SwiftUIPro/SwiftUIProApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProApp.swift
+//  SwiftUIPro
+//
+//  Created by 石玉龙 on 2021/1/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
